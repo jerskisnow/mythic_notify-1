106e6f905381272278292cb5506fabb0b4e6c3c3
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-07-15 17:29:45 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
+fx_version 'adamant'
+game 'gta5'
 name 'Mythic Framework Notification System'
 author 'Alzar - https://github.com/Alzar'
 version 'v1.1.0'
